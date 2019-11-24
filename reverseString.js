eeb2d10924f50486a8698b399cd9b9e09ac16e2a
Maksim Severukhin <mousemckill@gmail.com>
2019-11-24 14:51:16 +0300

    feat: task 1.1 implemented by readline

---
+++ reverseString.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin
+});
+
+rl.on('line', input => {
+  const reversed = input
+    .split('')
+    .reverse()
+    .join('');
+
+  console.log(`${reversed}`);
+});
+
+rl.on('close', () => {
+  process.exit(0);
+});

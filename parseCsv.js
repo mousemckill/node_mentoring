2a4783a1d527a79d8eb6cc4d5c3dc92b3dc0d10f
Maksim Severukhin <mousemckill@gmail.com>
2019-12-01 18:55:01 +0300

    feat: task 1.2 version 2

--- parseCsv.js
+++ parseCsv.js
@@ -1,26 +1,33 @@
-const { pipeline } = require('stream');
-const path = require('path');
-const fs = require('fs');
-const csv = require('csvtojson');
+const path = require("path");
+const { createReadStream, createWriteStream } = require("fs");
+const csv = require("csvtojson");
 
 const csvFilePath = path.resolve(
-	'csv',
-	'node_mentoring_t1_2_input_example.csv'
+  "csv",
+  "node_mentoring_t1_2_input_example.csv"
 );
 const txtFilePath = path.resolve(
-	'csv',
-	'node_mentoring_t1_2_input_example.txt'
+  "csv",
+  "node_mentoring_t1_2_input_example.txt"
 );
 
-pipeline(
-	fs.createReadStream(csvFilePath),
-	csv(),
-	fs.createWriteStream(txtFilePath),
-	(err) => {
-		if (err) {
-			console.error(err);
-		} else {
-			console.log('Success!');
-		}
-	}
-);
+const input = createReadStream(csvFilePath);
+const output = createWriteStream(txtFilePath);
+
+input.on("error", () => console.error("Input file not Found!"));
+
+csv({
+  headers: ["book", "author", "amount", "price"],
+  ignoreColumns: /amount/i
+})
+  .fromStream(input)
+  .subscribe(
+    jsonData => {
+      return new Promise((resolve, reject) => {
+        output.write(JSON.stringify(jsonData) + "\n", reject);
+        resolve();
+      });
+    },
+    e => console.error(e),
+    () => console.log("Finished")
+  );

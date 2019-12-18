328995ea9e792530a4dc37206d1e72d79881f5ad
Maksim Severukhin <maksim_severukhin@epam.com>
2019-12-18 16:15:18 +0300

    feat: added validation

--- src/UserApi.ts
+++ src/UserApi.ts
@@ -1,76 +1,152 @@
 import { Router, Request, Response, NextFunction } from "express";
 import uuid from "uuid";
+import {
+  createValidator,
+  ExpressJoiError,
+  ValidatedRequestSchema,
+  ContainerTypes,
+  ValidatedRequest
+} from "express-joi-validation";
 import { UserType } from "./UserType";
 import { autosuggest } from "./utils/autosuggest";
 import { findUserById } from "./utils/findUser";
+import { idSchema, bodySchema, loginSubstringSchema } from "./validationSchema";
 
 const UserApi = Router();
 
 const userList: UserType[] = [];
 
-UserApi.get("/users/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const user = findUserById(userList, id);
+const validator = createValidator({ passError: true });
 
-  if (!user) {
-    res.status(400).json({ message: "User not found" });
-  }
-
-  res.status(200).json(user);
-});
-
-UserApi.post("/users", (req: Request, res: Response) => {
-  const { login, password, age } = req.body;
-
-  const newUser: UserType = {
-    id: uuid.v4(),
-    login,
-    password,
-    age,
-    isDeleted: false
+interface UserIdParamRequest extends ValidatedRequestSchema {
+  [ContainerTypes.Params]: {
+    id: string;
   };
+}
 
-  userList.push(newUser);
+interface UserBodyRequest extends ValidatedRequestSchema {
+  [ContainerTypes.Body]: {
+    login: string;
+    password: string;
+    age: number;
+  };
+}
 
-  res.status(200).json(newUser);
-});
+interface UserSuggestParamRequest extends ValidatedRequestSchema {
+  [ContainerTypes.Params]: {
+    loginSubstring: string;
+  };
+  [ContainerTypes.Query]: {
+    limit: number;
+  };
+}
 
-UserApi.put("/users/:id", (req: Request, res: Response) => {
-  const { login, password, age } = req.body;
-  const { id } = req.params;
-  const user = findUserById(userList, id);
+interface UserIdParamAndBodyRequest extends UserBodyRequest, UserBodyRequest {}
 
-  if (!user) {
-    res.status(400).json({ message: "User not found" });
-  } else {
-    user.login = login;
-    user.age = Number(age);
-    user.password = password;
+UserApi.get(
+  "/users/:id",
+  validator.params(idSchema),
+  (req: ValidatedRequest<UserIdParamRequest>, res: Response) => {
+    const { id } = req.params;
+    const user = findUserById(userList, id);
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
   }
+);
 
-  res.status(200).json(user);
-});
+UserApi.post(
+  "/users",
+  validator.body(bodySchema),
+  (req: ValidatedRequest<UserBodyRequest>, res: Response) => {
+    const { login, password, age } = req.body;
 
-UserApi.delete("/users/:id", (req: Request, res: Response) => {
-  const { id } = req.params;
-  const user = findUserById(userList, id);
+    const newUser: UserType = {
+      id: uuid.v4(),
+      login,
+      password,
+      age,
+      isDeleted: false
+    };
 
-  if (!user) {
-    res.status(400).json({ message: "User not found" });
-  } else {
-    user.isDeleted = true;
+    userList.push(newUser);
+
+    res.status(200).json(newUser);
   }
+);
 
-  res.status(200).json(user);
-});
+UserApi.put(
+  "/users/:id",
+  validator.params(idSchema),
+  validator.body(bodySchema),
+  (req: ValidatedRequest<UserIdParamAndBodyRequest>, res: Response) => {
+    const { login, password, age } = req.body;
+    const { id } = req.params;
+    const user = findUserById(userList, id);
 
-UserApi.get("/suggest/:loginSubstring", (req: Request, res: Response) => {
-  const { limit } = req.query;
-  const { loginSubstring } = req.params;
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    } else {
+      user.login = login;
+      user.age = Number(age);
+      user.password = password;
+    }
 
-  const result = autosuggest(userList, loginSubstring, limit);
+    res.status(200).json(user);
+  }
+);
 
-  res.status(200).json(result);
-});
+UserApi.delete(
+  "/users/:id",
+  validator.params(idSchema),
+  (req: ValidatedRequest<UserIdParamRequest>, res: Response) => {
+    const { id } = req.params;
+    const user = findUserById(userList, id);
+
+    if (!user) {
+      res.status(400).json({ message: "User not found" });
+    } else {
+      user.isDeleted = true;
+    }
+
+    res.status(200).json(user);
+  }
+);
+
+UserApi.get(
+  "/suggest/:loginSubstring",
+  validator.params(loginSubstringSchema),
+  (req: ValidatedRequest<UserSuggestParamRequest>, res: Response) => {
+    const { limit } = req.query;
+    const { loginSubstring } = req.params;
+
+    const result = autosuggest(userList, loginSubstring, limit);
+
+    res.status(200).json(result);
+  }
+);
+
+UserApi.use(
+  (
+    err: any | ExpressJoiError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    if (err && err.error && err.error.isJoi) {
+      const e: ExpressJoiError = err;
+
+      res.status(400).json({
+        type: e.type,
+        message: e.error.toString()
+      });
+    } else {
+      next(err);
+    }
+  }
+);
 
 export { UserApi };
---
+++ src/validationSchema.ts
@@ -0,0 +1,50 @@
+import * as Joi from "@hapi/joi";
+
+export const idSchema = Joi.object({
+  id: Joi.string()
+    .guid({ version: ["uuidv4"] })
+    .required()
+});
+
+const password: any = Joi.extend(joi => ({
+  type: "alphabetAndNumbers",
+  base: joi.string(),
+  messages: {
+    "passwordComplexity.alphabet": '"{{#label}}" must be contain some chars',
+    "passwordComplexity.number": '"{{#label}}" must be contain some numbers'
+  },
+  validate(value, helpers) {
+    console.log({ value });
+    if (!/[a-zA-Z]+/.test(value)) {
+      return {
+        value,
+        errors: helpers.error("passwordComplexity.alphabet")
+      };
+    }
+
+    if (!/\d+/.test(value)) {
+      return { value, errors: helpers.error("passwordComplexity.number") };
+    }
+  }
+}));
+
+export const bodySchema = Joi.object({
+  login: Joi.string()
+    .min(2)
+    .required(),
+  password: password.alphabetAndNumbers().required(),
+  age: Joi.number()
+    .min(4)
+    .max(130)
+    .required()
+});
+
+export const loginSubstringSchema = Joi.object({
+  loginSubstring: Joi.string().required()
+});
+
+export const querySchema = Joi.object({
+  limit: Joi.number()
+    .min(1)
+    .required()
+});
